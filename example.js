ffa88a455034d78a384e66a19ad63d463cc5001d
Pivotsecurity <github@pivotsecurity.com>
2019-05-01 15:23:58 +0100

    metadata api update

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ account.env('f6074a20b7be4199b3300adf9a2c6efd','d48f21a6e0c94880b61daa2b9e5a2327
 
 account.Info('A13', '').on('complete',  function(data, response) {
 	if (response.statusCode == 200){
-	 console.log(data[0]);
+	 console.log(data);
 	 }else{
 	 	// error
 	 }
