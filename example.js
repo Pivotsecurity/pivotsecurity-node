70af0756083513d2bceb7bc3fe7723007a3bd243
Pivotsecurity <github@pivotsecurity.com>
2019-05-06 12:24:07 +0100

    Session update

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
-var account = require('./index');
+var api = require('./index');
 
-account.env('f6074a20b7be4199b3300adf9a2c6efd','d48f21a6e0c94880b61daa2b9e5a2327');
+api.env('f6074a20b7be4199b3300adf9a2c6efd','d48f21a6e0c94880b61daa2b9e5a2327');
 
-account.Info('A13', '').on('complete',  function(data, response) {
+api.Info('A13', '').on('complete',  function(data, response) {
 	if (response.statusCode == 200){
 	 console.log(data);
 	 }else{
@@ -10,7 +10,7 @@ account.Info('A13', '').on('complete',  function(data, response) {
 	 }
 });
 
-account.Verify('A13', '', '123124').on('complete',  function(data, response) {
+api.Verify('A13', '', '123124').on('complete',  function(data, response) {
 	if (response.statusCode == 200){
 	 	console.log(data);
 	 }else{
